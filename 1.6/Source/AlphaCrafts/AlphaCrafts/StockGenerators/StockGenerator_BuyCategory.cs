71322868eb674ba45452ca5f1286cb9bfa8ece61
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-06-24 15:52:36 +0200

    v2.0

---
+++ 1.6/Source/AlphaCrafts/AlphaCrafts/StockGenerators/StockGenerator_BuyCategory.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+namespace AlphaCrafts
+{
+    public class StockGenerator_BuyCategory : StockGenerator
+    {
+        public ThingCategoryDef category;
+
+        public override IEnumerable<Thing> GenerateThings(PlanetTile forTile, Faction faction = null)
+        {
+            return Enumerable.Empty<Thing>();
+        }
+
+        public override bool HandlesThingDef(ThingDef thingDef)
+        {
+            if (thingDef.thingCategories != null)
+            {
+                return thingDef.thingCategories.Contains(category);
+            }
+            return false;
+        }
+
+        public override Tradeability TradeabilityFor(ThingDef thingDef)
+        {
+            if (thingDef.tradeability == Tradeability.None || !HandlesThingDef(thingDef))
+            {
+                return Tradeability.None;
+            }
+            return Tradeability.Sellable;
+        }
+    }
+}
---
+++ 1.6/Source/AlphaCrafts/AlphaCrafts/WorldComponents/WorldComponent_ArtisanTradingQuests.cs
@@ -0,0 +1,66 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+using Verse;
+using RimWorld.QuestGen;
+using AlphaCrafts;
+
+namespace AlphaCrafts
+{
+
+
+
+    public class WorldComponent_ArtisanTradingQuests : WorldComponent
+    {
+        public int tickCounter;
+        public int ticksToNextQuest = 60000 * 14;
+
+
+        public WorldComponent_ArtisanTradingQuests(World world) : base(world)
+        {
+        }
+
+        public override void WorldComponentTick()
+        {
+            base.WorldComponentTick();
+
+            if (!AlphaCrafts_Mod.settings.AC_DisableQuests)
+            {
+
+                if (tickCounter > ticksToNextQuest)
+                {
+
+                    Slate slate = new Slate();
+
+                    if (InternalDefOf.AC_ArtisanTradeRequest.CanRun(slate, Find.CurrentMap))
+                    {
+                        Quest quest = QuestUtility.GenerateQuestAndMakeAvailable(InternalDefOf.AC_ArtisanTradeRequest, slate);
+
+                        QuestUtility.SendLetterQuestAvailable(quest);
+                        ticksToNextQuest = (int)(60000 * Rand.RangeInclusive(15, 30) * AlphaCrafts_Mod.settings.AC_QuestRate);
+                    }
+                   
+                    tickCounter = 0;
+
+
+
+
+                }
+                tickCounter++;
+            }
+
+
+
+
+
+
+
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref this.tickCounter, nameof(this.tickCounter));
+            Scribe_Values.Look(ref this.ticksToNextQuest, nameof(this.ticksToNextQuest));
+        }
+    }
+}
